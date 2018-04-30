c0f7ce3f03c9a8b7237fe94fb241d0f310695bba
jijianchun <jijianchun@kedacom.com>
2018-04-30 22:43:56 +0800

    初始化代码

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '123'
+    }
+  },
+  mounted () {
+    this.$store.dispatch('setUser', {name: 'james'})
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
